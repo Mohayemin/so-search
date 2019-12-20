f0ae22e71ad9f2cd385fa2bfd6f93b5fe9c0100f
Mohayemin <mohayeminul.islam@gmail.com>
2019-12-20 21:17:05 +0600

    add empty question list compoent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuestionListComponent } from './question-list/question-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'question',
+  component: QuestionListComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
