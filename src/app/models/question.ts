7814ce4c8fbe11ddb2adb6a5ace53931d76ad1e6
Mohayemin <mohayeminul.islam@gmail.com>
2019-12-21 20:44:54 +0600

    show bounty amount

--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -2,13 +2,14 @@ import { Answer } from './answer';
 import { Post } from './post';
 
 export interface Question extends Post {
-    question_id: number,
-    tags: string[],
-    is_answered: boolean,
-    answer_count: number,
-    view_count: number,
-    link: string,
-    title: string,
+    question_id: number;
+    tags: string[];
+    is_answered: boolean;
+    answer_count: number;
+    view_count: number;
+    link: string;
+    bounty_amount: number;
+    title: string;
     answers: Answer[];
 }
 
